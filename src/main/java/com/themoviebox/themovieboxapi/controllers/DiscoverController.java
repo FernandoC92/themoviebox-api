628afe91a13dac467f662647bad4853b04d869f2
Fernando Cordeiro <fernandoagnus1@gmail.com>
2020-04-13 23:02:40 -0300

    getMostPopular Method

--- src/main/java/com/themoviebox/themovieboxapi/controllers/DiscoverController.java
+++ src/main/java/com/themoviebox/themovieboxapi/controllers/DiscoverController.java
@@ -1,5 +1,7 @@
 package com.themoviebox.themovieboxapi.controllers;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import com.themoviebox.themovieboxapi.models.DataBase;
@@ -7,7 +9,6 @@ import com.themoviebox.themovieboxapi.models.Movie;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 // import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,22 +18,32 @@ public class DiscoverController {
     // /discover/movie?sort_by=popularity.desc
 
     @GetMapping
-    public List<Movie> getMostPopular(@RequestParam(name="sort_by") String sort_by) {
+    public List<Movie> getMostPopular() {
 
         List<Movie> response = new ArrayList<Movie>();
-        List<Movie> result = new ArrayList<Movie>();
+        // List<Movie> result = new ArrayList<Movie>();
         DataBase dataBase = new DataBase();
         response = dataBase.generateListMovies();
 
-        System.out.println("sort_by: " + sort_by);
+        // System.out.println("sort_by: " + sort_by);
+        
+        Collections.sort(response, new SortByPopularity());
 
         response.forEach(obj ->{
-            
+            System.out.println(obj);
         });
 
 
         return response;
 
+    }
+
+}
+
+class SortByPopularity implements Comparator<Movie> {
+    
+    public int compare(Movie a, Movie b) {
+        return Double.compare(b.getPopularity(), a.getPopularity());
     }
 
 }
